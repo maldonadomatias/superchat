ab2f9117065fd8750299b13bd756f643c6f326f0
Matias Agustin Maldonado <matiasagustinmaldonado@Matiass-MacBook-Pro-2.local>
2022-08-03 11:20:29 -0300

    1.1

--- src/components/Auth/ProtectedRoute.js
+++ src/components/Auth/ProtectedRoute.js
@@ -1,10 +1,16 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import Spinner from "../UI/Spinner";
 
 export function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  if (loading) return <h1>Loading</h1>;
+  if (loading)
+    return (
+      <div style={{display: 'flex', justifyContent: 'center', textAlign: 'center'}}>
+        <Spinner />
+      </div>
+    );
 
   if (!user) return <Navigate to="/login" />;
 
